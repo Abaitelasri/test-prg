6f97a55a67851ae97f31cc0f1dfe4039f259cc4f
Anas Agouriane <anas.agouriane@it.devcorp.fr>
2023-05-03 10:43:03 +0100

    4

--- connection.js
+++ connection.js
@@ -6,7 +6,7 @@ var mysqlConnection = mysql.createPool({
    host:process.env.DB_HOST,
    user:process.env.DB_USERNAME,
    password:process.env.DB_PASSWORD,
-   database:process.env.DB_DBNAME?
+   database:process.env.DB_DBNAME,
    waitForConnection: true,
    queueLimit:0
 });
