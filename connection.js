37bd4f23b772eee81e9cba6fac95f080de9e9659
Anas Agouriane <anas.agouriane@it.devcorp.fr>
2023-05-03 10:39:26 +0100

    third

--- connection.js
+++ connection.js
@@ -2,11 +2,13 @@ const mysql = require('mysql2');
 
 
 console.log('Connecting to database...');
-var mysqlConnection = mysql.createConnection({
-   host:'localhost',
-   user:'root',
-   password:'',
-   database:'test'
+var mysqlConnection = mysql.createPool({
+   host:process.env.DB_HOST,
+   user:process.env.DB_USERNAME,
+   password:process.env.DB_PASSWORD,
+   database:process.env.DB_DBNAME?
+   waitForConnection: true,
+   queueLimit:0
 });
 
 mysqlConnection.connect((err)=>{
