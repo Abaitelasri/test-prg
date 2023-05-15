3710206c850f2d5c927a9d872008f6f22e5dc6d5
Anas Agouriane <anas.agouriane@it.devcorp.fr>
2023-05-15 15:13:29 +0100

    meta

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const moment = require('moment');
 // add middleware to parse request body
 app.use(express.json());
 
-app.get('/ee', (req, res) => {
-  connection.query('SELECT * FROM people', (err, rows) => {
+app.get('/perDate', (req, res) => {
+  connection.query('SELECT DATE(date) as date ,count(id) as count_per_day FROM people group by DATE(date)', (err, rows) => {
     if (err) {
       console.error('Error executing query:', err);
       return;
@@ -18,52 +18,89 @@ app.get('/ee', (req, res) => {
     res.send(rows);
   });
 });
-app.post('/test',(req,res)=>{
-    console.log("worked");
-  
-      const sql = `INSERT INTO people (id,date,etat) VALUES ('11123','2023-05-03','aze')`;
+// requete !!!!
+//last Hour requete : SELECT COUNT(id) as count_last_hour FROM people WHERE date >= DATE_SUB(NOW(), INTERVAL 1 HOUR)
+//last week requete : SELECT COUNT(id) as count_last_week FROM people WHERE created_at >= DATE_SUB(NOW(), INTERVAL 1 WEEK);
+//last mounth requete :SELECT COUNT(id) as count_last_month FROM people WHERE created_at >= DATE_SUB(NOW(), INTERVAL 1 MONTH);
+
+
+
+
+
+
+app.get('/perHour', (req, res) => {
+  connection.query('SELECT DATE_FORMAT(date, "%H:00") as date ,count(id) as count_per_hour  FROM people group by HOUR(date)', (err, rows) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return;
+    }
+    console.log('Query result:', rows);
+    res.send(rows);
+  });
+});
+
+app.post('/bathroom',(req,res)=>{
+
+     
+
+      let id;
+      let dir;
+      let a;
+      if (req.body.meta[0].nodes[0].lines[0].cur_dir1_objects[0]) {
+        id = req.body.meta[0].nodes[0].lines[0].cur_dir1_objects[0];
+        dir = 'dir1';
+        a="The bathroom isn't empty";
+      
+      } else {
+        id = req.body.meta[0].nodes[0].lines[0].cur_dir2_objects[0];
+        dir = 'dir2';
+        a="The bathroom is empty";
+      }
+
+      const timestamp = new Date(req.body.meta[0].timestamp).toISOString();
+
+    const sql = `INSERT INTO people (id,date,etat,place) VALUES ('${id}','${timestamp}','${dir}','${req.body.meta[0].nodes[0].lines[0].label}')`;
   console.log('SQL query:', sql);
   connection.query(sql, (err, result) => {
     if (err) {
       console.error(err);
-      res.status(500).send(err);
+      res.status(500).send('Error inserting data into database'+err);
       return;
     }
     console.log('Data inserted successfully');
-    res.send('Data inserted successfully');
+    res.send(a);
   });
-    
 });
 
 
 // route for inserting data into the database
-app.post('/insertData', (req, res) => {
+app.post('/door', (req, res) => {
   console.log('Request body:', req.body);
 
   var a;
   var dir;
-  if (req.body.meta[0].nodes[0].lines[0].cur_dir1_objects[0]) {
-    a = req.body.meta[0].nodes[0].lines[0].cur_dir1_objects[0];
+  if (req.body.meta[0].nodes[0].lines[1].cur_dir1_objects[0]) {
+    a = req.body.meta[0].nodes[0].lines[1].cur_dir1_objects[0];
     dir = 'dir1';
   } else {
-    a = req.body.meta[0].nodes[0].lines[0].cur_dir2_objects[0];
+    a = req.body.meta[0].nodes[0].lines[1].cur_dir2_objects[0];
     dir = 'dir2';
   }
 
   const timestamp = moment(req.body.meta[0].timestamp).format('YYYY-MM-DD HH:mm:ss');
 
 
-  console.log('Inserting data into database...');
-  const sql = `INSERT INTO people (id,date,etat) VALUES ('${a}','${timestamp}','${dir}')`;
+  console.log(a);
+  const sql = `INSERT INTO people (id,date,etat,place) VALUES ('${a}','${timestamp}','${dir}','${req.body.meta[0].nodes[0].lines[1].label}')`;
   console.log('SQL query:', sql);
   connection.query(sql, (err, result) => {
     if (err) {
       console.error(err);
-      res.status(500).send('Error inserting data into database');
+      res.status(500).send('Error inserting data into database'+err);
       return;
     }
     console.log('Data inserted successfully');
-    res.send('Data inserted successfully');
+    res.send('person crossed');
   });
 });
 
